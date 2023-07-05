11c4200548dc3139bfa8cf92c23fdeb8ac799b2a
Bruce Lopez <lopez.bruce@gmail.com>
2023-07-05 07:50:37 -0400

    Initial Commit

---
+++ TimeConversion/TimeConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeConversionApp.swift
+//  TimeConversion
+//
+//  Created by Bruce Lopez on 7/5/23.
+//
+
+import SwiftUI
+
+@main
+struct TimeConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
