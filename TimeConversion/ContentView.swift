f0c0805681d02e8ae3308aa51207efe7ad8b4c37
Bruce Lopez <lopez.bruce@gmail.com>
2023-07-05 14:56:14 -0400

    Time Unit Converter

--- TimeConversion/ContentView.swift
+++ TimeConversion/ContentView.swift
@@ -8,14 +8,79 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    @State private var inputTime: Int?
+    @State private var convertionRate = 60
+    @FocusState private var timeIsFocused: Bool
+    @State private var selectedInputUnit = "Minutes"
+    @State private var timeUnit = ["Seconds", "Minutes", "Hours"]
+    @State private var selectedOutputUnit = "Seconds"
+    
+    var conversion: Double {
+        let inputValue = Double(inputTime ?? 0)
+        var tempConveted: Double
+
+        if selectedInputUnit == "Minutes" {
+            tempConveted =  inputValue * Double(convertionRate)
+        } else if selectedInputUnit == "Hours" {
+            tempConveted =  inputValue * Double(convertionRate) * Double(convertionRate)
+        } else {
+            tempConveted = inputValue
         }
-        .padding()
+        
+        return tempConveted
+    }
+    
+    var outputConversion: String {
+    var tempOutput: Double = conversion
+        if selectedOutputUnit == "Minutes" {
+            tempOutput =  conversion / Double(convertionRate)
+        } else if selectedOutputUnit == "Hours" {
+            tempOutput =  conversion / ( Double(convertionRate) * Double(convertionRate))
+        }
+        return String(format: "%g", tempOutput)
+    }
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Enter time", value: $inputTime, format: .number)
+                        .keyboardType(.numberPad).focused($timeIsFocused)
+                    
+                    Picker("Time unit selector?", selection: $selectedInputUnit) {
+                                   ForEach(timeUnit, id: \.self) {
+                                       Text($0)
+                                   }
+                               }
+                               .pickerStyle(.segmented)
+                } header: {
+                    Text("What time unit would you like to convert?")
+                    
+                }
+                Section {
+                    Text("\(outputConversion) \(selectedOutputUnit)")
+                    Picker("Time unit selectored?", selection: $selectedOutputUnit) {
+                                   ForEach(timeUnit, id: \.self) {
+                                       Text($0)
+                                   }
+                               }
+                               .pickerStyle(.segmented)
+                }header: {
+                    Text("Output time unit")
+                    
+                }
+            }
+            .navigationTitle("TimeConvert+")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    
+                    Button("Done") {
+                        timeIsFocused = false
+                    }
+                }
+            }
+        }
     }
 }
 
